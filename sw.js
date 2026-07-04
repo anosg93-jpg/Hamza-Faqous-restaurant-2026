d68579da40c364863ce58a4d8b6ed65230fb4be1
anosg93-jpg <anosg93@gmail.com>
2026-07-04 18:11:10 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'hamza-cache-v1';
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME)));
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(res => {
+            return res || fetch(e.request).then(networkRes => {
+                return caches.open(CACHE_NAME).then(cache => {
+                    cache.put(e.request, networkRes.clone());
+                    return networkRes;
+                });
+            });
+        })
+    );
+});
