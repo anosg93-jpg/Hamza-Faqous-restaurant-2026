7a2cf92fc58276827390d8b516da3eacb27933ae
anosg93-jpg <anosg93@gmail.com>
2026-07-04 18:58:49 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hamza-cache-v3';
+const CACHE_NAME = 'hamza-cache-v4';
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME)));
 self.addEventListener('fetch', e => {
     e.respondWith(
